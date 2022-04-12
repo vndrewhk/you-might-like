710a2cc7c7cc92e715da9b32acb4a223151fb4c1
vndrewhk <drewhkm@gmail.com>
2022-04-12 15:49:46 -0400

    search bar

--- components/Main/MainBody.jsx
+++ components/Main/MainBody.jsx
@@ -1,10 +1,17 @@
 import SearchBox from "../Search/SearchBox";
 
 const MainBody = () => {
+  const submitArtistHandler = (searchInput, e) => {
+    e.preventDefault();
+    console.log(e)
+    console.log(searchInput.current.value);
+    console.log("hi");
+  };
+
   return (
     <div className="main-body">
       Main Body
-      <SearchBox></SearchBox>
+      <SearchBox submitArtistHandler={submitArtistHandler}></SearchBox>
     </div>
   );
 };
--- components/Search/SearchBox.jsx
+++ components/Search/SearchBox.jsx
@@ -1,5 +1,39 @@
-const SearchBox = () => {
-  return <div>Search box</div>;
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Autocomplete from "@mui/material/Autocomplete";
+import AutocompleteChoices from "./AutocompleteChoices";
+import { FormControl } from "@mui/material";
+import { useRef } from "react";
+
+const SearchBox = (props) => {
+  const searchInput = useRef(null);
+  const submitArtistHandler = props.submitArtistHandler;
+
+  return (
+    <div>
+      <Stack spacing={2} sx={{ width: 300 }}>
+        <form onSubmit={submitArtistHandler.bind(null, searchInput)}>
+          <Autocomplete
+            freeSolo
+            id="free-solo-2-demo"
+            disableClearable
+            options={AutocompleteChoices.map((option) => option.title)}
+            renderInput={(params) => (
+              <TextField
+                inputRef={searchInput}
+                {...params}
+                label="Search Artist..."
+                InputProps={{
+                  ...params.InputProps,
+                  type: "search",
+                }}
+              />
+            )}
+          />
+        </form>
+      </Stack>
+    </div>
+  );
 };
 
 export default SearchBox;
