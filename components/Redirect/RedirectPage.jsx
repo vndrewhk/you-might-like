327f484149c4e4a731a177fe076f341dae025be7
vndrewhk <drewhkm@gmail.com>
2022-04-12 20:32:11 -0400

    local storage of access key

--- components/Redirect/RedirectPage.jsx
+++ components/Redirect/RedirectPage.jsx
@@ -5,12 +5,24 @@ const RedirectPage = () => {
   const router = useRouter();
   const pid = router.asPath;
   console.log(router.asPath);
+  //   console.log(pid.split("access_token=")[1]);
+  let accessToken = pid.split("access_token=")[1];
+
+  if (accessToken) {
+    accessToken = accessToken.split("&token_type");
+  }
+  console.log(accessToken);
+  //   console.log(typeof accessToken);
+  //   pid.split("access_token=");
+  //   pid.split("&token_type=");
+  //   pid.split("expires_in=");
+  console.log(pid);
   //   store asPath in localstorage
   useEffect(() => {
     localStorage.clear();
-    localStorage.setItem("access_token", "test");
-    localStorage.setItem("token_type", "test");
-    localStorage.setItem("expires_in", "test");
+    localStorage.setItem("access_token", `${accessToken[0]}`);
+    localStorage.setItem("token_type", "Bearer");
+    localStorage.setItem("expires_in", "3600");
     router.replace("/");
   }, []);
   return <div>Please waiting...redirecting..</div>;
